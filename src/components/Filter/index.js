4b01b8d52df08135b4ba10d61614ac528a1a8608
qqqqqq <qch_web@163.com>
2021-01-27 22:37:42 +0800

    完成找房页面标题渲染

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -2,6 +2,25 @@ import React, { Component } from 'react';
 import FilterDropdown from "../FilterDropdown";
 import "./index.scss";
 
+const filterTitleData = [
+    {
+        id: 1,
+        title: "区域"
+    },
+    {
+        id: 2,
+        title: "方式"
+    },
+    {
+        id: 3,
+        title: "租金"
+    },
+    {
+        id: 4,
+        title: "筛选"
+    }
+];
+
 export default class Filter extends Component {
 
     state = {
@@ -15,9 +34,34 @@ export default class Filter extends Component {
     }
 
     onConfirm = () => {
-        console.log(1);
         this.setState({
             openDrop: false
+        });
+    }
+
+    // 渲染下拉标题
+    renderTitle = () => {
+        return filterTitleData.map(item => {
+            let activeArrows = "iconfont icon-jiantouarrow486";
+            let activeTextColor = "filter_title"
+            if (this.state.openDrop) {
+                activeArrows = "iconfont icon-jiantouarrow486 title_active";
+                activeTextColor = "filter_title title_active"
+            }
+            return <div
+                key={item.id}
+                className={activeTextColor}
+                onClick={() => this.setState({ openDrop: !this.state.openDrop })}
+            >
+                <span>{item.title}</span>
+                <i className={activeArrows} style={
+                    {
+                        display: "inline-block",
+                        transform: this.state.openDrop ? [`rotate(${180}deg)`] : [`rotate(${0}deg)`],
+                        transition: " all .3s"
+                    }
+                }></i>
+            </div>
         });
     }
 
@@ -25,22 +69,7 @@ export default class Filter extends Component {
         return (
             <div className="filter">
                 <div className="title">
-                    <div className="filter_title title_active" onClick={() => this.setState({ openDrop: true })}>
-                        <span>区域</span>
-                        <i className="iconfont icon-jiantouarrow486 title_active"></i>
-                    </div>
-                    <div className="filter_title">
-                        <span>区域</span>
-                        <i className="iconfont icon-jiantouarrow486"></i>
-                    </div>
-                    <div className="filter_title">
-                        <span>区域</span>
-                        <i className="iconfont icon-jiantouarrow486"></i>
-                    </div>
-                    <div className="filter_title">
-                        <span>区域</span>
-                        <i className="iconfont icon-jiantouarrow486"></i>
-                    </div>
+                    {this.renderTitle()}
                 </div>
                 <FilterDropdown className="dropdown"
                     onOpenChange={this.onOpenChange}
